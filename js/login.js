294fcb5608a36ee3f38a9613a3990d79bbefc162
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-09-18 23:13:10 +0600

    Payoo mfs project's login page started

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById("button-login").addEventListener("click", function (e) {
+  e.preventDefault();
+  const phoneNumber = document.getElementById("phone-number").value;
+  console.log(phoneNumber);
+});
