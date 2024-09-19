3c7a100bdd95fc3cd9de78f8b99c77d73471bd42
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-09-19 12:07:15 +0600

    Working with home page

--- js/login.js
+++ js/login.js
@@ -1,5 +1,12 @@
 document.getElementById("button-login").addEventListener("click", function (e) {
   e.preventDefault();
   const phoneNumber = document.getElementById("phone-number").value;
-  console.log(phoneNumber);
+  const pinNumber = document.getElementById("pin-number").value;
+
+  if (phoneNumber === "5" && pinNumber === "1234") {
+    console.log("You are logged in");
+    window.location.href = "/home.html";
+  } else {
+    alert("Wrong phone number or password");
+  }
 });
